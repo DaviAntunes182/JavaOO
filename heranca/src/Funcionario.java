9e0f26908dc1921dd884639ae4b2311a92b1bb4c
DaviAntunes182 <davi.antunes@bb.com.br>
2024-10-06 14:46:01 -0300

    Herança -> Extends

---
+++ heranca/src/Funcionario.java
@@ -0,0 +1,38 @@
+import java.math.BigDecimal;
+
+public class Funcionario {
+    private String nome;
+    private String cpf;
+    private BigDecimal salario;
+
+    public Funcionario() {
+    }
+
+    public BigDecimal getBoniticacao(){
+        return this.salario.multiply(new BigDecimal("0.1"));
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public BigDecimal getSalario() {
+        return salario;
+    }
+
+    public void setSalario(String salario) {
+        this.salario = new BigDecimal(salario);
+    }
+}
---
+++ heranca/src/Principal.java
@@ -0,0 +1,13 @@
+public class Principal {
+    public static void main(String[] args) {
+        Funcionario nico = new Funcionario();
+
+        nico.setNome("Nico Steppat");
+        nico.setCpf("123456789-9");
+        nico.setSalario("2600.00");
+
+        System.out.println(nico.getNome());
+        System.out.println(nico.getBoniticacao());
+
+    }
+}
